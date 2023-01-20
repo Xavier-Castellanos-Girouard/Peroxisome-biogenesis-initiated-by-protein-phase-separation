b02bebcc6d66529f830488649c85c129966bdeec
Xavier-Castellanos-Girouard <105664447+Xavier-Castellanos-Girouard@users.noreply.github.com>
2023-01-19 20:08:09 -0500

    Add files via upload

---
+++ Video_Analysis/Video_5/Fusion_2/pex_droplet_fusion.R
@@ -0,0 +1,117 @@
+# Visualizing droplet coalescence data for liquid-liquid phase separation
+# Xavier Castellanos-Girouard, Rini Ravindran Patel
+# August 17 2022
+
+
+# Import libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+
+
+
+# Import values
+aspect_ratio_raw <- read.csv(file = "aspect_ratio_values.csv", header = FALSE)
+major_axis_raw <- read.csv(file = "axis_major_values.csv", header = FALSE)
+
+
+# Make dataframe from values in pertinent range
+aspect_ratio <- data.frame(values = as.numeric(aspect_ratio_raw[1,420:480]))
+
+# Creating time points
+timepoints <- seq(0, (nrow(aspect_ratio)-1)*0.03, by = 0.03)
+
+# Adding time points to dataframe
+aspect_ratio$time = timepoints
+aspect_ratio$major_axis = as.numeric(major_axis_raw[1,420:480])
+
+
+# Make the plot
+plot <- ggplot(data = aspect_ratio,
+         mapping = aes(x= time,
+                       y= values)) +
+  geom_point() +
+  theme_bw() +
+  xlab("Time (s)") +
+  ylab("Aspect Ratio")
+
+dev.new()
+plot
+
+pdf("Figure5C_panel1.pdf") 
+plot
+dev.off()
+
+#### Fitting curve ####
+
+
+# Keep only the points in the decay curve
+time_slice_start = 19
+#which(aspect_ratio$time == 0.66)
+
+time_slice_end = 43
+#which(aspect_ratio$time == 1.32)
+
+print(paste("Curve Start:", time_slice_start, " | ", "Curve End:", time_slice_end))
+
+# Take the points slice:
+analysis_slice_df = aspect_ratio[c(time_slice_start:time_slice_end),]
+
+
+# plot the slice
+
+# Make the plot
+plot <- ggplot() +
+  geom_point(data = analysis_slice_df,
+             mapping = aes(x= time,
+                           y= values)) +
+  theme_bw() +
+  xlab("Time") +
+  ylab("Aspect Ratio")
+
+dev.new()
+plot
+
+## Defining the decay curve
+
+# Plot the linear decay slope
+initial_aspect_ratio = aspect_ratio$values[time_slice_start] # initial AR
+ini_t = aspect_ratio$time[time_slice_start] # Initial time start of decay
+final_AR = aspect_ratio$value[time_slice_end] # Final AR
+
+e_num = exp(1) # Eulers number
+
+# Decay Function
+eDecay <- function(t, IniAR,tau){final_AR+(IniAR-final_AR)*(e_num**(-(t-ini_t)/tau))}
+
+# Plot decay functio
+plot(eDecay(t = (analysis_slice_df$time), IniAR = initial_aspect_ratio, 0.06034))
+
+
+
+# Make model to find fit tau
+fit_model1 <- 
+  nls(values ~ eDecay(time, initial_aspect_ratio, tau), 
+      data = analysis_slice_df, 
+      start = list(tau = 0.06034)) # 0.06034
+
+print(fit_model1) # 0.06034
+
+
+# Plot model and experimental data
+plot2 <- ggplot() +
+  geom_point(data = analysis_slice_df,
+             mapping = aes(x= time,
+                           y= values)) +
+  geom_function(fun = eDecay,
+                args = list(IniAR = initial_aspect_ratio,
+                            tau = 0.06034),
+                color = "red") +
+  theme_bw() +
+  xlab("Time") +
+  ylab("Aspect Ratio")
+
+pdf("Figure5C_panel2.pdf")
+plot2
+dev.off()
